360fc91188700fe82c75b66d48efa252683fd5c7
Gabriel Beli <gabrielimabelo@gmail.com>
2024-06-18 14:49:59 +0100

    alg

--- FichaPratica/src/FichaPratica03/ex07.java
+++ FichaPratica/src/FichaPratica03/ex07.java
@@ -1,7 +1,18 @@
 package FichaPratica03;
 
+import java.util.Scanner;
+
 public class ex07 {
     public static void main(String[] args) {
 
+        int number1 = 5, count = 0, total;
+
+        while (number1 >= count ){
+            System.out.print(count);
+            count = count + 1;
+        }
+        total = count + count;
+
+        System.out.print(total);
     }
 }
