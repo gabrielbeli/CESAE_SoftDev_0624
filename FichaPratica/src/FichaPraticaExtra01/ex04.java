ae42f5c321932c4cde37b6eb2368823dfee3a29e
Gabriel Beli <145936985+gabrielbeli@users.noreply.github.com>
2024-06-20 00:16:10 +0100

    ex04

--- FichaPratica/src/FichaPraticaExtra01/ex04.java
+++ FichaPratica/src/FichaPraticaExtra01/ex04.java
@@ -9,27 +9,17 @@ public class ex04 {
         String alpha;
 
         System.out.print("Insira uma letra: ");
-        alpha = input.next();
+        alpha = input.next().toLowerCase();
 
-        if (alpha.equals("a")) {
-            System.out.println("é uma vogal");
-        }
-        if (alpha.equals("e")) {
-            System.out.println("é uma vogal");
-        }
-        if (alpha.equals("i")) {
-            System.out.println("é uma vogal");
-        }
-        if (alpha.equals("o")) {
-            System.out.println("é uma vogal");
-        }
-        if (alpha.equals("u")) {
-            System.out.println("é uma vogal");
-        }
-        else {
-            System.out.println("é uma consoante");
+        if (alpha.length() == 1 && Character.isLetter(alpha.charAt(0))) {
+            switch (alpha){
+                case "a", "e", "i", "o", "u" ->
+                    System.out.println("é uma vogal");
+                default ->
+                    System.out.println("é uma consoante");
+        }} else {
+            System.out.println("Entrada inválida. Por favor, insira uma única letra.");
         }
     }
-
 }
 
